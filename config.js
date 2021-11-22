15619b0d086d53a2da8bcd2d123bf7fd7d87e1c9
CVOvidiu <cvovidiu2002@gmail.com>
2021-11-22 15:22:17 +0200

    Slash commands integration

--- config.js
+++ config.js
@@ -1,7 +1,44 @@
-const { Client } = require("discord.js")
+const fs = require('fs')
+const { Client, Collection } = require("discord.js")
 
 // Create new client instance
 const client = new Client({intents:['GUILDS', 'GUILD_MESSAGES', 'GUILD_INVITES', 'GUILD_MEMBERS', 'DIRECT_MESSAGES'], partials: ['CHANNEL']})
+
+// Event-files handling
+const eventFiles = fs.readdirSync('./events').filter((file) => file.endsWith('.js'))
+for(const file of eventFiles) {
+    const event = require(`./events/${file}`)
+    if(event.once)
+        client.once(event.name, (...args) => event.execute(...args))
+    else
+        client.on(event.name, (...args) => event.execute(...args))
+}
+
+// Command-files handling
+client.commands = new Collection()
+const commandFiles = fs.readdirSync('./commands').filter((file) => file.endsWith('.js'))
+for(const file of commandFiles) {
+    const command = require(`./commands/${file}`)
+    client.commands.set(command.data.name, command)
+}
+
+// Slash commands
+client.on('interactionCreate', async interaction => {
+    if(!interaction.isCommand())
+        return
+    
+    const command = client.commands.get(interaction.commandName)
+
+    if(!command)
+        return
+
+    try {
+        await command.execute(interaction)
+    } catch(error) {
+        console.error(error)
+        await interaction.reply({content: 'Error!'})
+    }
+})
 
 // When bot is ready
 client.on('ready', () => {
